543278bc9714e796cc831ab365a50208dd9ef950
DzmitryHalubovich <buxarik493@gmail.com>
2024-04-24 12:56:47 +0300

    replace OfficeResponseDTO class by record

--- src/Services/Offices/Offices.Contracts/DTOs/OfficeResponseDTO.cs
+++ src/Services/Offices/Offices.Contracts/DTOs/OfficeResponseDTO.cs
@@ -1,10 +1,4 @@
 ﻿namespace Offices.Contracts.DTOs;
 
-public class OfficeResponseDTO
-{
-    public string Id { get; set; }
-    public string Address { get; set; }
-    public string Photo_Id { get; set; }
-    public string Registry_phone_number { get; set; }
-    public bool IsActive { get; set; }
-}
+public record OfficeResponseDTO(string Id, string Address, 
+    string Photo_Id, string Registry_phone_number, bool IsActive);
